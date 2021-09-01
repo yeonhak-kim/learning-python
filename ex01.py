5969b4cc7af86d732594343e92ca0f3c7d51a436
Yeonhak Kim <yeonhakk1101@gmail.com>
2021-09-01 09:38:51 -0400

    commit exercise 2: comments and pound characters

--- ex01.py
+++ ex01.py
@@ -1,1 +1,5 @@
+## Learn the Python the Hard Way ##
+## Exercise 1: A Good First Program
 print("Hello World!")
+
+# Personal Comment: learn how to use [VIM] 
---
+++ ex02.py
@@ -0,0 +1,8 @@
+## Exercise 2: Comments and Pound Characters
+
+# comments can be written starting with '#'
+print("Comment symbol is named 'octothorpe' or 'pound character'.")
+print("Pound character can be also used to diable certain line of code.")
+
+
+# Personal Comment: Try checking the code in reverse direction.
