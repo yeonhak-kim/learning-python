f0b2c4d9d448fa8762683ca97ca3cb6c5ef2e21d
Yeonhak Kim <yeonhakk1101@gmail.com>
2021-09-02 10:17:44 -0400

    commit exercise 3: Numbers and Math

---
+++ ex03.py
@@ -0,0 +1,25 @@
+## Exercise 03: Numbers and Math
+
+print("counting chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("counting eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3.0 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Is it False?")
+
+print("....More")
+
+print("Greater?", 5 > -2)
+print("Greater or equal?", 5 >= -2)
+print("Less or equal?", 5 <= -2)
